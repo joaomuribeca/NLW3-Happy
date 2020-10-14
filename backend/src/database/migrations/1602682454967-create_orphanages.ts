a97a2713f1224a4d77f3618da2bb2cfcc59bb261
João Muribeca <53870325+CheffJ@users.noreply.github.com>
2020-10-14 11:18:24 -0300

    Cadastro de orfanatos

--- backend/src/database/migrations/1602682454967-create_orphanages.ts
+++ backend/src/database/migrations/1602682454967-create_orphanages.ts
@@ -38,6 +38,10 @@ export class createOrphanages1602682454967 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
